22414da069050ce14b950cc58b0760ebb07c518d
AtwopatNigeria <support.atwopat@gmail.com>
2026-05-13 16:24:28 +0100

    Add form utility functions for password toggle and clear
    Added utility functions for toggling password visibility and clearing input fields.

---
+++ public_html/js/form-utils.js
@@ -0,0 +1,21 @@
+// form-utils.js
+
+function togglePassword() {
+
+  const passwordInput = document.getElementById("password");
+  const toggleIcon = document.getElementById("toggleIcon");
+
+  if(passwordInput.type === "password"){
+    passwordInput.type = "text";
+    toggleIcon.classList.remove("fa-eye-slash");
+    toggleIcon.classList.add("fa-eye");
+  }else{
+    passwordInput.type = "password";
+    toggleIcon.classList.remove("fa-eye");
+    toggleIcon.classList.add("fa-eye-slash");
+  }
+}
+
+function clearInput(id){
+  document.getElementById(id).value = "";
+}
